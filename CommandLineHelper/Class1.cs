771e402edbcfb8bef4b3241031ba9a55be0d6abc
Yumi Ta <34748443+Yumi08@users.noreply.github.com>
2018-07-06 19:27:45 -0400

    Add project files.

---
+++ CommandLineHelper/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommandLineHelper
+{
+    public class Class1
+    {
+    }
+}
